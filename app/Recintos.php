fc09f4367297c47f49f0e2ad52e4e78b4a34c13b
FredPeal <frederickpeal@gmail.com>
2020-04-06 19:04:04 -0400

    Nuevos coordinadores

--- app/Recintos.php
+++ app/Recintos.php
@@ -16,7 +16,14 @@ class Recintos extends Model
         'address'
     ];
     
-    protected $appends = ['number_colegios','coordinador'];
+    protected $appends = [
+        'number_colegios',
+        'coordinador',
+        'coordinador_ejecutivo',
+        'coordinador_electoral',
+        'coordinador_seguridad',
+        'coordinador_finanza'
+    ];
 
     protected $foreignKey = 'recintos_id';
 
@@ -56,6 +63,58 @@ class Recintos extends Model
                          ->join('users','votantes.id','=','users.votantes_id')
                          ->join('roles','roles.id','=','users.roles_id')
                          ->where('roles.name','Coordinador de Recinto')
+                         ->where("votantes.{$this->foreignKey}", $this->id)
+                         ->select('votantes.*')
+                         ->first();
+
+        return $coordinador ?  $coordinador->first_name. ' '.$coordinador->last_name : ' ';
+    }
+
+    public function getCoordinadorEjecutivoAttribute()
+    {
+        $coordinador = self::join('votantes', "{$this->table}.id",'=','votantes.'.$this->foreignKey)
+                         ->join('users','votantes.id','=','users.votantes_id')
+                         ->join('roles','roles.id','=','users.roles_id')
+                         ->where('roles.name','Ejecutivo')
+                         ->where("votantes.{$this->foreignKey}", $this->id)
+                         ->select('votantes.*')
+                         ->first();
+
+        return $coordinador ?  $coordinador->first_name. ' '.$coordinador->last_name : ' ';
+    }
+
+    public function getCoordinadorElectoralAttribute()
+    {
+        $coordinador = self::join('votantes', "{$this->table}.id",'=','votantes.'.$this->foreignKey)
+                         ->join('users','votantes.id','=','users.votantes_id')
+                         ->join('roles','roles.id','=','users.roles_id')
+                         ->where('roles.name','Electoral')
+                         ->where("votantes.{$this->foreignKey}", $this->id)
+                         ->select('votantes.*')
+                         ->first();
+
+        return $coordinador ?  $coordinador->first_name. ' '.$coordinador->last_name : ' ';
+    }
+
+    public function getCoordinadorSeguridadattribute()
+    {
+        $coordinador = self::join('votantes', "{$this->table}.id",'=','votantes.'.$this->foreignKey)
+                         ->join('users','votantes.id','=','users.votantes_id')
+                         ->join('roles','roles.id','=','users.roles_id')
+                         ->where('roles.name','Seguridad')
+                         ->where("votantes.{$this->foreignKey}", $this->id)
+                         ->select('votantes.*')
+                         ->first();
+
+        return $coordinador ?  $coordinador->first_name. ' '.$coordinador->last_name : ' ';
+    }
+
+    public function getCoordinadorFinanzaAttribute()
+    {
+        $coordinador = self::join('votantes', "{$this->table}.id",'=','votantes.'.$this->foreignKey)
+                         ->join('users','votantes.id','=','users.votantes_id')
+                         ->join('roles','roles.id','=','users.roles_id')
+                         ->where('roles.name','Finanza')
                          ->where("votantes.{$this->foreignKey}", $this->id)
                          ->select('votantes.*')
                          ->first();
