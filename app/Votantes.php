55da430b5e9883076e84fd3a465425e7d2bfdfe3
FredPeal <frederickpeal@gmail.com>
2020-04-16 02:31:43 -0400

    Change computed name

--- app/Votantes.php
+++ app/Votantes.php
@@ -56,6 +56,6 @@ class Votantes extends Model
 
     public function getNameAttribute()
     {
-        return $this->card_id;
+        return "{$this->first_name} {$this->last_name} {$this->card_id}";
     }
 }
