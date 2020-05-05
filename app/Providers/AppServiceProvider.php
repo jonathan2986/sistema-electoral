0315effbe8b07a5d96d7cc649d25e388605280f3
FredPeal <frederickpeal@gmail.com>
2020-05-05 12:38:05 -0400

    Fix cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Circunscripciones;
+use App\ColegiosElectorales;
+use App\ComitesBases;
+use App\Distritos;
+use App\Municipios;
+use App\People;
+use App\Recintos;
+use Cache;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +23,33 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Circunscripciones::saved(function ($data) {
+            Cache::flush();
+        });
+
+        Municipios::saved(function ($data) {
+            Cache::flush();
+        });
+
+        Recintos::saved(function ($data) {
+            Cache::flush();
+        });
+
+        Distritos::saved(function ($data) {
+            Cache::flush();
+        });
+
+        ComitesBases::saved(function ($data) {
+            Cache::flush();
+        });
+
+        ColegiosElectorales::saved(function ($data) {
+            Cache::flush();
+        });
+
+        People::saved(function ($data) {
+            Cache::flush();
+        });
     }
 
     /**
